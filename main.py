28bb1a76cb37211c4671d3231bc015b5855c6ca5
ademirMaltazard <ademir.mk98@gmail.com>
2024-04-17 21:01:55 -0300

    começo do projeto da aula com streamlit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+print('console')
+
+st.title('minha primeira pagina :wolf:')
+
+st.write('''aosnfoiansiofnas  
+asibiabjfa  
+asknsoidvnoksdv
+sdbnvosmdvs
+ndsdkvnds usd vubdv ysbdnvusbndv us vujsdnvui snvsidunv sdiuvn sdnv sduv sdiuv dv  
+sd un sduhsd8ijhs divsd8j sidjv sdijv dsivjsdiojv sidjv iosj divj sdv
+''')
