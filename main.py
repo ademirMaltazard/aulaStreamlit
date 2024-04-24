894faccb0c36ea98d9d377a7382a72c44834ace6
ademirMaltazard <ademir.mk98@gmail.com>
2024-04-24 20:50:10 -0300

    adição de filtro com pandas e streamlit usando dataframe

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ print('console')
 
 st.title('minha primeira pagina :wolf:')
 
-st.write('''aosnfoiansiofnas  
-asibiabjfa  
-asknsoidvnoksdv
-sdbnvosmdvs
-ndsdkvnds usd vubdv ysbdnvusbndv us vujsdnvui snvsidunv sdiuvn sdnv sduv sdiuv dv  
-sd un sduhsd8ijhs divsd8j sidjv sdijv dsivjsdiojv sidjv iosj divj sdv
+st.write('''
+    Teste de escrita com comando write
 ''')
 df = pandas.read_csv('https://raw.githubusercontent.com/hermeson883/data_science_workshop/main/linguagens.csv')
 
 st.dataframe(df)
+
+result = st.selectbox('qual sua maneira de contato ideal:', ['telefone', 'celular', 'email'])
+
+st.write(result)
+
+
+
+linguagem = st.selectbox("selecione a linguagem", df['Linguagem'].unique())
+
+if st.button('Filtrar'):
+    df.loc[df["Linguagem"] == linguagem]
