cf06b229ec66b477795b8d1aaffa30db50ff8b9b
ademirMaltazard <ademir.mk98@gmail.com>
2024-05-01 15:00:30 -0300

    alterações do arquivo main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas
 
-print('console')
-
 st.title('minha primeira pagina :wolf:')
 
 st.write('''
@@ -19,21 +17,3 @@ linguagem = st.selectbox("selecione a linguagem", df['Linguagem'].unique())
 
 if st.button('Filtrar por linguagem'):
     df.loc[df["Linguagem"] == linguagem]
-
-#--------------------------------------------------------------------------------------
-
-st.write('## EXIBIÇÃO MICRODADOS SOBRE VIOLÊNCIA')
-
-csv_violence = pandas.read_csv("https://raw.githubusercontent.com/natorjunior/pandas/main/Aula-02/microdados_violencia_reduzido.csv")
-# df_violence = st.dataframe(csv_violence)
-
-uf = st.selectbox('selecione a UF:', csv_violence['id_uf_ocorrencia'].unique())
-button_fill = st.button('filtrar')
-
-if button_fill:
-    st.write('### dados filtrados por uf:')
-    fill_uf = csv_violence.loc[csv_violence['id_uf_ocorrencia'] == uf]
-    st.dataframe(fill_uf)
-
-    st.write('### dados filtrados por tipo de arma:')
-    st.dataframe(fill_uf.loc[fill_uf['meio_arma_fogo'] == 0])
