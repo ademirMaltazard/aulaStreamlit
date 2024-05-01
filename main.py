f391b93d2867c9cf35ff619b38cb4879afc8fa0e
ademirMaltazard <ademir.mk98@gmail.com>
2024-05-01 15:16:28 -0300

    atulaização da pagina para melhor explicar os testes

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ st.title('minha primeira pagina :wolf:')
 
 st.write('''
     Teste de escrita com comando write
+    
+    #### Exibindo dados:
 ''')
+
 df = pandas.read_csv('https://raw.githubusercontent.com/hermeson883/data_science_workshop/main/linguagens.csv')
 st.dataframe(df)
 
+st.write('#### Teste de selectbox')
 result = st.selectbox('qual sua maneira de contato ideal:', ['telefone', 'celular', 'email'])
-
 st.write(result)
 
+st.write('#### Primeiro uso de selectbox para filtragem de dados:')
 linguagem = st.selectbox("selecione a linguagem", df['Linguagem'].unique())
 
 if st.button('Filtrar por linguagem'):
